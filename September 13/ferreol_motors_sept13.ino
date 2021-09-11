26cb4774b01c2cb0987caaad7057d380672ffe2d
Katie Ferreol <kmf527@nyu.edu>
2021-09-11 23:14:53 +0400

    Update ferreol_motors_sept13.ino

--- September 13/ferreol_motors_sept13.ino
+++ September 13/ferreol_motors_sept13.ino
@@ -1,10 +1,15 @@
+//Katie Ferreol
+//Assignment 1: Push Button & Motors
+//Press the pushbutton in order to turn both motors.
+
+//Setting up the variables
 const int pushButton = 2;
 const int in1Pin = 7;
 const int in2Pin = 6;
 const int in3Pin = 5;
 const int in4Pin = 4;
 
-
+//Declaring the inputs and outputs
 void setup() {
   Serial.begin(9600);
   pinMode(pushButton, INPUT);
@@ -15,11 +20,13 @@ void setup() {
 }
 
 void loop() {
-  
+
+//  Reading the state of the button
   int buttonState = digitalRead(pushButton);
   Serial.println(buttonState);
   delay(1);
 
+//  If button is pressed, turn the motors, if not, stop them
   if (buttonState == HIGH) {
     digitalWrite(in1Pin, LOW);
     digitalWrite(in2Pin, HIGH);
