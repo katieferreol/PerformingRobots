aa39335bee4a740fc44d58ea920e54db5ca92de8
Katie Ferreol <kmf527@nyu.edu>
2021-10-05 15:48:41 +0400

    Add files via upload

---
+++ Midterm Project/midterm_transmit.ino
@@ -0,0 +1,55 @@
+const int CEPIN = 9;
+const int CSNPIN = 10;
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(CEPIN, CSNPIN);                // CE, CSN
+
+const byte address[6] = "00001";
+
+const int stoicPin = 1;
+const int happyPin = 2;
+const int sadPin = 3;
+const int angryPin = 4;
+
+int value = 0;
+
+//declaring states of buttons
+int stoicState = 0;
+int happyState = 0;
+int sadState = 0;
+int angryState = 0;
+
+//variables needed for state reading
+int stoicPrevious = 0;
+int stoicPush = 0;
+int happyPrevious = 0;
+int happyPush = 0;
+int sadPrevious = 0;
+int sadPush = 0;
+int angryPrevious = 0;
+int angryPush = 0;
+
+void setup() {
+  Serial.begin(115200);
+  boolean retval = radio.begin();   //Starting the Wireless communication
+  Serial.println(retval);
+  radio.openWritingPipe(address);  //destination addres
+  radio.setPALevel(RF24_PA_MIN);   // min or max
+  radio.stopListening();           //This sets the module as transmitter
+}
+
+void loop() {
+    int stoic = digitalRead(stoicPin) << 1;
+    int happy = digitalRead(happyPin) << 2;
+    int sad = digitalRead(sadPin) << 3;
+    int angry = digitalRead(angryPin) << 4;
+  
+  int data = stoic | happy | sad | angry;
+  if (data) {
+    Serial.print( "sending data = " );
+    Serial.println(data);
+    radio.write(&data, sizeof(data)) ;
+  }
+}
